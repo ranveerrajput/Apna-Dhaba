fda081930138ec90ccf698727d00af876853ccf3
ranveerrajput <ranveerrajput351@gmail.com>
2023-04-09 09:43:48 +0530

    Lecture 5 done.Lets get Hooked. Used useState() hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
 import { restaurantList } from "../config";
 import RestaurantCard from "./RestaurantCard";
 
+function filterDate(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurants) =>
+    restaurants.data.name.includes(searchText)
+  );
+  return filterData;
+}
+
 const Body = () => {
+  const [searchText, setSearchText] = useState();
+  const [restaurants, setRestaurants] = useState(restaurantList);
   return (
     <>
       <div className="search-container">
@@ -9,12 +19,23 @@ const Body = () => {
           type="text"
           className="search-input"
           placeholder="Search"
-          value=""
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
         />
-        <button className="search-btn">Search</button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterDate(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
       </div>
       <div className="restaurant-list">
-        {restaurantList.map((restaurent) => {
+        {restaurants.map((restaurent) => {
           return (
             <RestaurantCard {...restaurent.data} key={restaurent?.data?.id} />
           );
